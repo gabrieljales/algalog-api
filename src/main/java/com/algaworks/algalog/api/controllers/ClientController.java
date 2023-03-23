888f35e59a95d85e9c107a5baa72eca8e9e266c8
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-03-22 22:08:53 -0300

    feat: Using Jakarta Persistence

--- src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
@@ -4,27 +4,20 @@ import com.algaworks.algalog.domain.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
+    @PersistenceContext // Injeta um EntityManager na variável manager
+    private EntityManager manager;
+
     @GetMapping("/clients")
     public List<Client> list() {
-        Client client1 = new Client();
-        client1.setId(1L);
-        client1.setName("João");
-        client1.setPhone("34 99999-1111");
-        client1.setEmail("joao@algalog.com");
-
-        // TODO: Parei no vídeo 1 do capítulo 2, aos 7 minutos
-
-        Client client2 = new Client();
-        client2.setId(2L);
-        client2.setName("Maria");
-        client2.setPhone("34 93333-1222");
-        client2.setEmail("maria@algalog.com");
-        return Arrays.asList(client1, client2);
+        return manager.createQuery("from Client", Client.class)
+                .getResultList();
     }
 }
--- src/main/java/com/algaworks/algalog/domain/model/Client.java
+++ src/main/java/com/algaworks/algalog/domain/model/Client.java
@@ -1,13 +1,25 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-// Geram os métodos getters e setters para as propriedades definidas na classe, evitando código boilerplate
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// Geram os métodos equals, hashCode, getters e setters para as propriedades definidas na classe, evitando código boilerplate
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // Só vai usar propriedades incluídas de forma explícita
 @Getter
 @Setter
+@Entity
 public class Client {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
