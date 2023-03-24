ecb55f0e1cd1cdd94b48a9dca9512689bc803eda
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-03-23 22:10:01 -0300

    fix: ClientRepository

--- src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
@@ -1,23 +1,21 @@
 package com.algaworks.algalog.api.controllers;
 
 import com.algaworks.algalog.domain.model.Client;
+import com.algaworks.algalog.domain.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor // Gerando um construtor com todas as propriedas
 @RestController
 public class ClientController {
 
-    @PersistenceContext // Injeta um EntityManager na variável manager
-    private EntityManager manager;
+    private ClientRepository clientRepository;
 
     @GetMapping("/clients")
     public List<Client> list() {
-        return manager.createQuery("from Client", Client.class)
-                .getResultList();
+        return clientRepository.findAll();
     }
 }
---
+++ src/main/java/com/algaworks/algalog/domain/repository/ClientRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algalog.domain.repository;
+
+import com.algaworks.algalog.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // Componente do spring (Tipo onde as instâncias desse tipo são gerenciadas pelo container do spring)
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    // Método customizado: Atente-se a usar um padrão de nomenclatura com prefixo válido (find..., query... e etc), um delimitador By e o nome da propriedade
+    List<Client> findByName(String name);
+    List<Client> findByNameContaining(String name); // Nome não exato, usando o LIKE do SQL
+}
