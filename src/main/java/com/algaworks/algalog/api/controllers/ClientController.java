8d73f5f340507fe2a2de2213833d2d8dba950278
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-03-27 20:43:14 -0300

    feat: find client by id

--- src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClientController.java
@@ -3,19 +3,31 @@ package com.algaworks.algalog.api.controllers;
 import com.algaworks.algalog.domain.model.Client;
 import com.algaworks.algalog.domain.repository.ClientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor // Gerando um construtor com todas as propriedas
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
     private ClientRepository clientRepository;
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> list() {
         return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id) {
+        return clientRepository.findById(id)
+                .map(ResponseEntity::ok)// Essa linha é a mesma coisa que: .map(client -> ResponseEntity.ok(client))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
