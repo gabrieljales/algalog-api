e2cfa4beac0d42d0f8c8098a4e27deb6dbffd2a8
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-06 16:18:09 -0300

    feat: Create RequestDeliveryService

---
+++ src/main/java/com/algaworks/algalog/domain/service/RequestDeliveryService.java
@@ -0,0 +1,33 @@
+// OBS1: Esse serviço poderia ser mais genérico e conter mais métodos, como todos os métodos de CRUD. Isso fica a critério do time
+// OBS2: Veja que aqui estamos usando a linguagem de domínio, ou seja, invés de ser "CreateDeliveryService", usamos uma linguagem que os usuários usam com "RequestDeliveryService" (solicitação de entrega)
+package com.algaworks.algalog.domain.service;
+
+import com.algaworks.algalog.domain.model.Client;
+import com.algaworks.algalog.domain.model.Delivery;
+import com.algaworks.algalog.domain.model.DeliveryStatus;
+import com.algaworks.algalog.domain.repository.DeliveryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Service
+public class RequestDeliveryService {
+    private ClientCatalogService clientCatalogService;
+    private DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public Delivery request(Delivery delivery) {
+        // OBS: Nesse caso o mais indicado é um erro 400 invés de um 404, já que houve um erro ao passar um cliente inexistente no corpo
+        Client client = clientCatalogService.get(delivery.getClient().getId());
+
+        // Propriedades calculadas (não são informadas pelo usuário)
+        delivery.setClient(client); // Evita de retornar o cliente com propriedades vazias e somente o ID preenchido
+        delivery.setStatus(DeliveryStatus.PENDING);
+        delivery.setOrderDate(LocalDateTime.now());
+
+        return deliveryRepository.save(delivery);
+    }
+}
