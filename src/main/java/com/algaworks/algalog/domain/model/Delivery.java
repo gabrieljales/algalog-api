16b8effccc279d69ff37ad2289a059ba051da281
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-06 17:55:11 -0300

    feat: Validate client, recipient and tax properties

--- src/main/java/com/algaworks/algalog/domain/model/Delivery.java
+++ src/main/java/com/algaworks/algalog/domain/model/Delivery.java
@@ -1,11 +1,16 @@
 package com.algaworks.algalog.domain.model;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -19,12 +24,18 @@ public class Delivery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Valid // Validar o objeto client (cascata)
+    @ConvertGroup(from = Default.class, to = ValidationGroups.ClientId.class) // Converte de um grupo de validação para outro, invés de usar o Default usa o outro passado
+    @NotNull
     @ManyToOne // Muitas entregas -> Um cliente (padrão do nome da coluna mapeada: client_id. Caso queira usar outro padrão, usar o @JoinColumn)
     private Client client;
 
+    @Valid // Nesse caso, sem conversão (usa o Default group)
+    @NotNull
     @Embedded // Ausado para indicar que um objeto deve ser mapeado como um objeto incorporado em uma entidade (fica tudo na tabela de entrega)
     private Recipient recipient;
 
+    @NotNull
     private BigDecimal tax;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY) // Evitar que o cliente preencha esse campo
