3a1820c9830fa707c97605ffddee997c2cf16c8d
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-06 17:54:23 -0300

    feat: Create ValidationGroups

---
+++ src/main/java/com/algaworks/algalog/domain/ValidationGroups.java
@@ -0,0 +1,6 @@
+package com.algaworks.algalog.domain;
+
+// Interface para agrupar grupos de validação
+public interface ValidationGroups {
+    public interface ClientId { } // Não tem métodos por ser uma interface de marcação
+}
