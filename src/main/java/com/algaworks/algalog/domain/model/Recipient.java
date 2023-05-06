83712412acad18c453e4a0bf29652966ef62f3e2
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-06 17:57:29 -0300

    feat: Validate all properties with NotBlank

--- src/main/java/com/algaworks/algalog/domain/model/Recipient.java
+++ src/main/java/com/algaworks/algalog/domain/model/Recipient.java
@@ -5,23 +5,29 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Embeddable // Indica ser um objeto incorporado
 public class Recipient {
+    @NotBlank
     @Column(name = "recipient_name") // Evitar que o campo "name" referente ao destinatário seja confundido com o nome da entrega
     private String name;
 
+    @NotBlank
     @Column(name = "recipient_address")
     private String address;
 
+    @NotBlank
     @Column(name = "recipient_number")
     private  String number;
 
+    @NotBlank
     @Column(name = "recipient_complement")
     private String complement;
 
+    @NotBlank
     @Column(name = "recipient_district")
     private String district;
 }
