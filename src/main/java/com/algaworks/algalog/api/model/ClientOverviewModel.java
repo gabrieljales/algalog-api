c821f09444e203ea9f56a7f65192d16455a96cb6
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-12 19:16:50 -0300

    refactor: Create Model to Client object in DeliveryModel

---
+++ src/main/java/com/algaworks/algalog/api/model/ClientOverviewModel.java
@@ -0,0 +1,11 @@
+package com.algaworks.algalog.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClientOverviewModel {
+    private Long id;
+    private String name;
+}
--- src/main/java/com/algaworks/algalog/api/model/DeliveryModel.java
+++ src/main/java/com/algaworks/algalog/api/model/DeliveryModel.java
@@ -11,7 +11,7 @@ import java.time.OffsetDateTime;
 @Setter
 public class DeliveryModel {
     private Long id;
-    private String clientName;
+    private ClientOverviewModel client;
     private RecipientModel recipient;
     private BigDecimal tax;
     private DeliveryStatus status;
