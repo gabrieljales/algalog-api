698d43a30526a4bc08ef31c66b0c3809d40e366c
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-12 19:19:41 -0300

    refactor: Complete separation between Domain and Representation Model - Delivery

--- src/main/java/com/algaworks/algalog/api/controllers/DeliveryController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/DeliveryController.java
@@ -1,7 +1,8 @@
 package com.algaworks.algalog.api.controllers;
 
+import com.algaworks.algalog.api.mapper.DeliveryMapper;
 import com.algaworks.algalog.api.model.DeliveryModel;
-import com.algaworks.algalog.api.model.RecipientModel;
+import com.algaworks.algalog.api.model.input.DeliveryInputModel;
 import com.algaworks.algalog.domain.model.Delivery;
 import com.algaworks.algalog.domain.repository.DeliveryRepository;
 import com.algaworks.algalog.domain.service.RequestDeliveryService;
@@ -19,38 +20,31 @@ import java.util.List;
 public class DeliveryController {
     private DeliveryRepository deliveryRepository;
     private RequestDeliveryService requestDeliveryService;
+    private DeliveryMapper deliveryMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Delivery request(@Valid @RequestBody Delivery delivery) {
-        return requestDeliveryService.request(delivery);
+    public DeliveryModel request(@Valid @RequestBody DeliveryInputModel deliveryInputModel) {
+        // OBS importante: A conversão deve ser feita aqui no controller, o service não pode conhecer classes da camada da API
+
+        // Recebe um objeto do tipo DeliveryInputModel e mapeia para entidade Delivery
+        Delivery newDelivery = deliveryMapper.toEntity(deliveryInputModel);
+        // Chama o service que retorna uma Delivery
+        Delivery requestedDelivery = requestDeliveryService.request(newDelivery);
+
+        // Mapeia para DeliveryModel
+        return deliveryMapper.toModel(requestedDelivery);
     }
 
     @GetMapping
-    public List<Delivery> list() {
-        return deliveryRepository.findAll();
+    public List<DeliveryModel> list() {
+        return deliveryMapper.toCollectionModel(deliveryRepository.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DeliveryModel> findById(@PathVariable Long id) {
         return deliveryRepository.findById(id)
-                .map(delivery -> {
-                    DeliveryModel deliveryModel = new DeliveryModel();
-                    deliveryModel.setId(delivery.getId());
-                    deliveryModel.setClientName(delivery.getClient().getName());
-                    deliveryModel.setRecipient(new RecipientModel());
-                    deliveryModel.getRecipient().setName(delivery.getRecipient().getName());
-                    deliveryModel.getRecipient().setAddress(delivery.getRecipient().getAddress());
-                    deliveryModel.getRecipient().setComplement(delivery.getRecipient().getComplement());
-                    deliveryModel.getRecipient().setNumber(delivery.getRecipient().getNumber());
-                    deliveryModel.getRecipient().setDistrict(delivery.getRecipient().getDistrict());
-                    deliveryModel.setTax(delivery.getTax());
-                    deliveryModel.setStatus(delivery.getStatus());
-                    deliveryModel.setOrderDate(delivery.getOrderDate());
-                    deliveryModel.setDueDate(delivery.getDueDate());
-
-                    return ResponseEntity.ok(deliveryModel);
-                })
+                .map(delivery -> ResponseEntity.ok(deliveryMapper.toModel(delivery)))
                 .orElse(ResponseEntity.notFound().build());
     }
 }
