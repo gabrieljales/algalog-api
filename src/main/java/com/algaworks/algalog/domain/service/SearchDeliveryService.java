61126c49e857882926b42b2c6a95557512140548
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-05-25 16:39:20 -0300

    feat: Create SearchDeliveryService

---
+++ src/main/java/com/algaworks/algalog/domain/service/SearchDeliveryService.java
@@ -0,0 +1,19 @@
+package com.algaworks.algalog.domain.service;
+
+import com.algaworks.algalog.domain.exception.EntityNotFoundException;
+import com.algaworks.algalog.domain.model.Delivery;
+import com.algaworks.algalog.domain.repository.DeliveryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class SearchDeliveryService {
+
+    private DeliveryRepository deliveryRepository;
+
+    public Delivery search(Long deliveryId) {
+        return deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new EntityNotFoundException("Entrega não encontrada"));
+    }
+}
